623ee58fb4b58ebbf5d0ffaa74e2c51018ca3d6c
Dmitry Gruzd <donotsendhere@gmail.com>
2013-11-11 14:44:17 +0400

    seeds.rb

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -1,10 +1,20 @@
 require 'spec_helper'
 
 describe Member do
-  it 'member should not be valid without name' do
+
+  before(:each) do
     @member = FactoryGirl.build(:member)
+  end
+
+  it 'should not be valid without name' do
     @member.should be_valid
     @member.name = nil
+    @member.should_not be_valid
+  end
+
+  it 'should not be valid without website' do
+    @member.should be_valid
+    @member.website = nil
     @member.should_not be_valid
   end
 end
