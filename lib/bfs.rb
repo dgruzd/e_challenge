0ae91545fdfdf82fecca27baaee4e4fbdfcefb14
Dmitry Gruzd <donotsendhere@gmail.com>
2013-11-12 20:29:33 +0400

    Find path working

--- lib/bfs.rb
+++ lib/bfs.rb
@@ -1,25 +1,29 @@
 class BFS
-  attr_accessor :array, :visited, :queue
+  attr_accessor :array, :visited, :queue, :p, :result
   def initialize
-    q = Friendship.select('MAX(member_id) as m1, MAX(friend_id) as m2').order(nil).first
-    array_size = (q.m1 > q.m2 ? q.m1 : q.m2) + 1
+    q = Friendship.select('MAX(member_id) as m1, MAX(friend_id) as m2, MAX(id) as m3').order(nil).first
+    rows_count = (q.m1 > q.m2 ? q.m1 : q.m2) + 1
+    columns_count = q.m3 + 1
 
-    @array = Array.new(array_size){ Array.new(array_size){nil} }
+    @array = Array.new(rows_count){ Array.new(rows_count){0} }
     Friendship.find_each do |f|
       id1, id2 = f.member_id, f.friend_id
       @array[id1][id2] = 1
       @array[id2][id1] = 1
     end
+    
+    # @array = Array.new(rows_count){ Array.new(columns_count){0} }
+    # Friendship.find_each do |f|
+    #   id1, id2 = f.member_id, f.friend_id
+    #   @array[id1][f.id] = 1
+    #   @array[id2][f.id] = 1
+    # end
 
     @visited = Hash.new(false)
 
     @queue = []
-  end
-
-  def expand(node)
-    res = []
-    array[node].each_with_index{|v,i| res << i if v}
-    res
+    @p = Hash.new
+    @result = []
   end
 
   def bfs(start_node, *end_nodes)
@@ -27,20 +31,36 @@ class BFS
 
     queue.push(start_node)
     visited[start_node] = true
+    current_path = [start_node]
+
+    p[start_node] = nil
 
     while queue.present?
       node = queue.shift
+
       if end_nodes.include? node
-        return true
+        result << path(start_node, node)
       end
 
-      expand(node).each do |child|
+      array[node].each_with_index do |v,child|
+        next unless v == 1
         unless visited[child]
           queue.push(child)
           visited[child] = true
+          @p[child] = node
         end
       end
     end
-    return false
+    return result
+  end
+
+  def path(s, v)
+    res = []
+    while true
+      break if v.nil?
+      res << v
+      v = p[v]
+    end
+    res.reverse
   end
 end
